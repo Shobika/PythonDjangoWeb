451472f07abb14e389555cac96d36c52c8337cd5
Shobika <shobikaksr@gmail.com>
2019-12-29 03:04:37 -0500

    Update manage.py

--- DemoProject/manage.py
+++ DemoProject/manage.py
@@ -7,6 +7,8 @@ import sys
 def main():
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'DemoProject.settings')
     try:
+        from django.core.management.commands.runserver import Command as runserver
+    runserver.default_addr = "0.0.0.0"
         from django.core.management import execute_from_command_line
     except ImportError as exc:
         raise ImportError(
